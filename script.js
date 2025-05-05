348630d8eee1f0860d71e9eec16e246c729c2dcb
Kamanakashyap <kamanakashyap54@gmail.com>
2025-05-05 09:33:19 +0530

    change image next gen

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Welcome to Rhythm Dance Studio!");
-  });
-
-
-if("serviceWorker"in navigator){navigator.serviceWorker.register("/service-worker.js").then(e=>console.log("Service Worker registered:",e)).catch(e=>console.log("Registration failed:",e))}
-  
